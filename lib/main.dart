098214c9ac0244b910c29770390e22c799df4c16
trujunzhang <trujunzhang@gmail.com>
2020-06-08 14:23:56 +0800

    Instagram-Clone

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:ieatta/resources/repository.dart';
+import 'package:ieatta/ui/insta_home_screen.dart';
+import 'package:ieatta/ui/login_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() {
+    return new MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var _repository = Repository();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Instagram',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+            primarySwatch: Colors.blue,
+            primaryColor: Colors.black,
+            primaryIconTheme: IconThemeData(color: Colors.black),
+            primaryTextTheme: TextTheme(
+                title: TextStyle(color: Colors.black, fontFamily: "Aveny")),
+            textTheme: TextTheme(title: TextStyle(color: Colors.black))),
+        home: FutureBuilder(
+          future: _repository.getCurrentUser(),
+          builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+            if (snapshot.hasData) {
+              return InstaHomeScreen();
+            } else {
+              return LoginScreen();
+            }
+          },
+        ));
+  }
+}
